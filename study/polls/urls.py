b2dad11876dfcf3e681138fe828e18d47909e6d7
IonutGI <ganea.ionut.iulian@gmail.com>
2014-02-16 06:07:12 -0800

    Add new urls for CBV.

--- study/polls/urls.py
+++ study/polls/urls.py
@@ -6,8 +6,23 @@ Created on Feb 16, 2014
 
 from django.conf.urls import patterns, url
 
+from polls.views import PollDetail, PollIndex
+
 from . import views
 
+
 urlpatterns = patterns("",
-    url(r'^$', views.index, name="index")
+    #===========================================================================
+    # #FBV
+    #===========================================================================
+    url(r'^$', views.index, name="poll-index"),
+    url(r'(?P<poll_id>\d+)/$', views.detail, name="poll-detail"),
+    url(r'(?P<poll_id>\d+)/results/$', views.results, name="poll-results"),
+    url(r'(?P<poll_id>\d+)/vote/$', views.vote, name="poll-vote"),
+
+    #===========================================================================
+    # #CBS
+    #===========================================================================
+    url(r'index/$', PollIndex.as_view(), name="cbv-poll-index"),
+    url(r'(?P<pk>\d+)/detail/$', PollDetail.as_view(), name="cbv-poll-detail"),
 )
